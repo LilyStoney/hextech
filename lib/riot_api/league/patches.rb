02bb896a2bc2ab350c569e48d2826479971195da
Lily Stoney <lily@lilypad.info>
2021-07-28 15:35:54 +0100

    Move values require into adapter

--- lib/riot_api/league/patches.rb
+++ lib/riot_api/league/patches.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'open-uri'
-
 module RiotApi
   module League
     class Patches < RiotApi::Adapter
--- lib/riot_api/league/summoner.rb
+++ lib/riot_api/league/summoner.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'values'
-
 module RiotApi
   module League
     class Summoner < RiotApi::Adapter
