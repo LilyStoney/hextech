455774d3d09bba9aa8f3b327d3ad7c4fe5ad46a3
Lily Stoney <lily@lilypad.info>
2021-07-28 21:15:12 +0100

    Add ThirdPartyCode endpoint

---
+++ lib/riot_api/league/third_party_code.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module RiotApi
+  module League
+    class ThirdPartyCode < RiotApi::Adapter
+
+      include RiotApi::League::ResponseAttributes
+
+      def initialize(summoner_id:, region: 'euw1')
+        @summoner_id = summoner_id
+        @region = region
+      end
+
+      def call
+        response = send_request
+
+        raise ThirdPartyCodeError, 'There is no code for the given summoner or it expired' if response.not_found?
+
+        Response.with(third_party_code: response)
+      end
+
+      private
+
+      attr_reader :summoner_id, :region
+
+      def path
+        "https://#{region}.api.riotgames.com/lol/platform/v4/third-party-code/by-summoner/#{summoner_id}"
+      end
+
+      class Response < Value.new(:third_party_code); end
+
+    end
+  end
+end
