b6298df40dbfa5e6ee6bbd7531557fe5de8014bb
Lily Stoney <lily@lilypad.info>
2021-07-28 15:25:43 +0100

    Add Champion Rotation endpoint and a WIP changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [0.0.1] - 2021-07-28
+
+### Added
+
+- League endpoints
+  - API
+    - CHAMPION-MASTERY-V4
+    - CHAMPION-V3
+    - SUMMONER-V4
+  - CDN
+    - Champion Information
+    - Patch Information
+
+[0.0.1]: https://github.com/lilystoney/riot_api/releases/tag/v0.0.1
---
+++ lib/riot_api/league/champion_rotation.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'values'
+
+module RiotApi
+  module League
+    class ChampionRotation < RiotApi::Adapter
+
+      ATTRIBUTES = %i[
+        free_champion_ids
+        free_champion_ids_for_new_players
+        max_new_player_level
+      ].freeze
+
+      def initialize(region: 'euw1')
+        @region = region
+      end
+
+      def call
+        response = send_request
+
+        Response.with(response.transform_keys { |key| key.underscore.to_sym })
+      end
+
+      private
+
+      attr_reader :region
+
+      def path
+        "https://#{region}.api.riotgames.com/lol/platform/v3/champion-rotations"
+      end
+
+      class Response < Value.new(*RiotApi::League::ChampionRotation::ATTRIBUTES); end
+
+    end
+  end
+end
